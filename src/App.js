0314e7896c932395b4820e8c0f0c47d37b5ce1dc
Kai L <kailin8591@gmail.com>
2022-07-09 13:12:15 -0400

    chart.js and semantic intergation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './App.css';
+import {
+  Chart as ChartJS, ArcElement, Tooltip, Legend,
+} from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { Icon } from 'semantic-ui-react';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function App() {
   return (
     <div>
-      Testing
+      <Icon loading name="spinner" />
+      <Doughnut data={{
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      }}
+      />
     </div>
   );
 }
